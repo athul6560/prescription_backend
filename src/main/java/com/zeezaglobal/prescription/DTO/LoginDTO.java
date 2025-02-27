0c32e6a95021ae034353525f4c93d508b51027cd
Athul Babu <athul.6560@gmail.com>
2025-02-27 17:21:33 -0600

    login finished

--- src/main/java/com/zeezaglobal/prescription/DTO/LoginDTO.java
+++ src/main/java/com/zeezaglobal/prescription/DTO/LoginDTO.java
@@ -1,14 +1,12 @@
 package com.zeezaglobal.prescription.DTO;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class UserRequest {
-    private String username;
-    private String password;
+public class LoginDTO {
     private String email;
+    private String password;
 
 }
